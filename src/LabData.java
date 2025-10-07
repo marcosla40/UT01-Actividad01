4f91bf02f802e127db7d818a5b21478caa46ae13
marcosla40 <marcosla40@educastur.es>
2025-10-07 19:02:04 +0200

    Labdata copiao HashMap

--- src/LabData.java
+++ src/LabData.java
@@ -1,6 +1,24 @@
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 public class LabData implements Serializable {
+
+    final String ruta_pacientes = "./data/in/pacientes.csv";
+    final String ruta_tecnicos = "./data/in/tecnicos.tsv";
+    final String ruta_muestras = "./data/in/muestras.psv";
+
+    private HashMap<String, Paciente> pacientes;
+    private HashMap<String, Tecnico> tecnicos;
+    private List<Muestra> muestras;
+
+    public LabData() {
+        this.pacientes = new LinkedHashMap<>();
+        this.tecnicos = new LinkedHashMap<>();
+        this.muestras = new ArrayList<>();
+    }
 
     public void leerPacientes() {
 
